b8b31c2f0dc6238fd8d10a18e0a9656e60afc261
Zasha <zahid.shaikh@live.com>
2025-08-30 13:17:34 +0530

    created a new dist dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint:{
-        ignoreDuringBuilds: true
+        distDir: 'dist',
+        eslint: {
+            ignoreDuringBuilds: true
+        }
     }
-};
+
 export default nextConfig;
